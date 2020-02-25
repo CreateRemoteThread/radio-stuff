2290f177bcdb0d978a2519cf869cedc13b4b7c9f
CreateRemoteThread <elegura.wolfe@gmail.com>
2020-02-25 12:17:25 +1100

    fixeS

--- fft-snap.py
+++ fft-snap.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python3
 
-# UHD DEFAULTS TO ANTENNA 2
+# UHD defaults to antenna 2.
 
 import SoapySDR
 from SoapySDR import * #SOAPY_SDR_ constants
 import numpy #use numpy for buffers
 import matplotlib
-# import psd
-# matplotlib.use("Agg")
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from numpy import *
-
-#enumerate devices
-results = SoapySDR.Device.enumerate()
-for result in results: print(result)
-
-#create device instance
-#args can be user defined or from the enumeration result
-args = dict(driver="uhd")
-sdr = SoapySDR.Device(args)
+import getopt
+import sys
+import os
 
 CONFIG_SR = 10 * 1024 ** 2
-
-#query device info
-print(sdr.listAntennas(SOAPY_SDR_RX, 0))
-print(sdr.listGains(SOAPY_SDR_RX, 0))
-freqs = sdr.getFrequencyRange(SOAPY_SDR_RX, 0)
-for freqRange in freqs: print(freqRange)
-
-# _psd = psd.PSD(bins,CONFIG_SR,fft_window=8192,fft_overlap=0.5,crop_factor=False,
+CONFIG_FREQS = []
+CONFIG_FREQS.append(88 * 1024 ** 2) # FM Band
+CONFIG_FREQS.append(98 * 1024 ** 2)
+CONFIG_FREQS.append(108 * 1024 ** 2)
+CONFIG_FREQS.append(423 * 1024 ** 2) # 433Mhz Band
+CONFIG_FREQS.append(433 * 1024 ** 2)
+CONFIG_FREQS.append(443 * 1024 ** 2)
+CONFIG_PREFIX = "output/fft"
+CONFIG_DRIVER = "uhd"
 
 def doFrequencyFFT(sdr,center):
   sdr.setSampleRate(SOAPY_SDR_RX, 0, CONFIG_SR)
@@ -41,6 +35,41 @@ def doFrequencyFFT(sdr,center):
   sdr.deactivateStream(rxStream) #stop streaming
   sdr.closeStream(rxStream)
   ax.specgram(buff[0:sr.ret],NFFT=256,Fs=CONFIG_SR,Fc=center,noverlap=128)
-  plt.show()
+  plt.savefig("%s-%d.png" % (CONFIG_PREFIX,center))
 
-doFrequencyFFT(sdr,95 * 1024 ** 2)
+def usage():
+  print("-h: display this message")
+  print("-d: select driver (default: uhd, antenna 2)")
+  print("-f: add a frequency (default: 88,433 + 20 either way)")
+  print("-w: specify the write prefix (default: fft)")
+  sys.exit(0)
+
+CONFIG_GAIN = None
+
+def saneUnits(str_freq):
+  if str_freq[-1] in ('m','M'):
+    return int(strfreq[0:-1]) * 1024 ** 2
+  else:
+    return int(str_freq)
+
+if __name__ == "__main__":
+  args,opts  = getopt.getopt(sys.argv[1:],"hd:f:w:g:",["help","driver=","freq=","writefile=","gain="])
+  for opt,arg in args:
+    if opt in ("-h","--help"):
+      usage()
+    elif opt in ("-d","--driver"):
+      CONFIG_DRIVER = arg
+    elif opt in ("-w","--writefile"):
+      CONFIG_PREFIX = arg
+    elif opt in ("-f","--freq"):
+      CONFIG_FREQS.append(saneUnits(arg))
+    elif opt in ("-g","--gain"):
+      CONFIG_GAIN = float(arg)
+  if CONFIG_PREFIX == "output/fft":
+    os.system("mkdir output")
+  args = dict(driver="uhd")
+  sdr = SoapySDR.Device(args)
+  if CONFIG_GAIN is not None:
+    sdr.setGain(SOAPY_SDR_RX, 0, CONFIG_GAIN)
+  for freq in CONFIG_FREQS:
+    doFrequencyFFT(sdr,freq)
